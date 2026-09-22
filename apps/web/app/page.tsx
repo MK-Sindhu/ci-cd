b80bc1c4595cf1b5a345e2dcfa9436394719363a
MK_Sindhu <1234mohitsindhu@gmail.com>
2026-09-23 03:13:43 +0530

    added nginx conf

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "@repo/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const user = await db.orm.public.User.select("id", "username").all().first();
 
